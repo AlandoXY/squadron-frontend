acd474d9723c4b74f0c877b12e6465858cab6acf
Roxanne199 <142577031+Roxanne199@users.noreply.github.com>
2023-09-14 20:55:17 +0800

    Create tab.tsx

---
+++ components/tab.tsx
@@ -0,0 +1,32 @@
+import styled from "styled-components";
+import React, {useContext} from "react";
+import {TabsContext} from "@/components/tabs";
+
+const TabContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border-bottom: ${({active, color}) => active ? `3px solid ${color}` : "3px solid transparent"};
+  padding: 10px 25px;
+  cursor: pointer;
+  color: ${({active, color}) => active ? color : "initial"};;
+  
+  &:hover {
+    color: ${props => props.color};
+  }
+`;
+
+interface TabProps {
+  children: React.ReactNode;
+  value?: string;
+  color?: string;
+}
+
+export default function Tab({ children, value, color = "#6941C6" }: TabProps) {
+  const { value: parentValue, onChange } = useContext(TabsContext);
+
+  return (
+    <TabContainer color={color} active={parentValue === value} onClick={() => onChange(value || "")}>
+      {children}
+    </TabContainer>
+  );
+}
