58aead6b5d501f5f0fc321febe9c4b5d90d6ecaf
Aloys <992773074@qq.com>
2023-09-22 01:48:16 +1000

    Updated components

--- components/progress-bar.tsx
+++ components/progress-bar.tsx
@@ -6,8 +6,9 @@ const ProgressBarContainer = styled.div`
   overflow: hidden;
   display: block;
   height: ${({height}) => height + 'px'};
-  background-color: ${props => props.color + '77'};
+  background-color: ${({color, bgColor}) => bgColor || color + '77'};
   border-radius: 5px;
+  width: 100%;
 `;
 
 const ProgressBarInner = styled.div`
@@ -26,12 +27,13 @@ interface ProgressBarProps {
   value?: number;
   total?: number;
   color?: string;
+  bgColor?: string;
   height?: number;
 }
 
-export default function ProgressBar({ total = 100, value = 0, color = "#1976d2", height = 4 }: ProgressBarProps) {
+export default function ProgressBar({ total = 100, value = 0, color = "#1976d2", bgColor, height = 4 }: ProgressBarProps) {
   return (
-    <ProgressBarContainer color={color} height={height}>
+    <ProgressBarContainer color={color} height={height} bgColor={bgColor}>
       <ProgressBarInner total={total} value={value} color={color} />
     </ProgressBarContainer>
   );
