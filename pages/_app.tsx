cdaebc370c7f59f85c22bf370238fa5cd6a9b158
AlandoXY <43557631+AlandoXY@users.noreply.github.com>
2023-09-22 02:11:22 +1000

    Update mock

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,10 @@
 import type { AppProps } from "next/app";
 import { ThemeProvider, DefaultTheme } from "styled-components";
 import GlobalStyle from "@/components/globalstyles";
+import {useEffect, useState} from "react";
+import {initMocks} from "@/mocks";
+
+require('../mocks')
 
 const theme: DefaultTheme = {
   colors: {
@@ -10,11 +14,21 @@ const theme: DefaultTheme = {
 };
 
 export default function App({ Component, pageProps }: AppProps) {
+  const [isMock, setIsMock] = useState(false)
+
+  useEffect(() => {
+    initMocks().then(() => {
+      setIsMock(true);
+    })
+  }, [])
+
   return (
     <>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
-        <Component {...pageProps} />
+        {
+          isMock && <Component {...pageProps} />
+        }
       </ThemeProvider>
     </>
   );
